919e5f598f47d590e8c1709fe11b43d420d55c7a
Jai Sharma <sendtojsharma@gmail.com>
2020-09-13 13:16:24 +0530

    using an staic image (not needed)

---
+++ src/script.js
@@ -0,0 +1,14 @@
+// const img = document.getElementById('dog');
+
+// const fetchRandomDogImage = () => {
+//     fetch('https://dog.ceo/api/breeds/image/random')
+//         .then(res => res.json())
+//         .then(data => {
+//             img.src = data.message; // setting the image url
+//         })
+// }
+
+// fetchRandomDogImage();
+
+/* ====================================================================== */
+
